ec4531c19e539409c318cefc800590f0719a18ca
Laureano <lauregalan1@gmail.com>
2025-11-11 19:25:06 -0300

    Add fonts

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="w-full p-8 bg-white text-black">
+        <header className="w-full p-10 bg-white text-black">
             <h1>Como le fue mal?</h1>
         </header>
     );
