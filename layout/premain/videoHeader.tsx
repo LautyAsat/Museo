a8c0318f72cb111e86e8c0fc928c857871bdf044
Lautaro Asat <lautaronorielasat@gmail.com>
2025-11-18 17:00:39 -0300

    Fix videoHeader style

--- layout/premain/videoHeader.tsx
+++ layout/premain/videoHeader.tsx
@@ -6,7 +6,7 @@ export default function VideoHeader() {
       muted
       loop
       playsInline
-      className="w-full h-screen pb-20 object-cover object-center -translate-y-44"
+      className="w-full h-[80vh] pb-20 object-cover object-center"
     />
   );
 }
